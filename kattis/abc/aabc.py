135027ad56ec8af09f9dfbdff8cee929ae4eeb3a
Zeke Barefoot <syncorex0@gmail.com>
2023-10-10 20:06:18 -0600

    Testing for 1st problem

--- kattis/abc/aabc.py
+++ kattis/abc/aabc.py
@@ -1,7 +1,9 @@
 import sys
 from typing import List
 
-def answer(nums:List[int], order:str) -> str:
+
+def answer(nums: List[int], order: str) -> str:
+
     nums.sort()
     result = ['', '', '']
     for i in range(3):
@@ -9,7 +11,9 @@ def answer(nums:List[int], order:str) -> str:
         result[i] = str(nums[j])
     return ' '.join(result)
 
+
 if __name__ == "__main__":
+
     nums = [int(x) for x in sys.stdin.readline().split()]
     order = sys.stdin.readline()
     print(answer(nums, order))
--- kattis/abc/test_unit_abc.py
+++ kattis/abc/test_unit_abc.py
@@ -11,26 +11,34 @@ class TestABC(unittest.TestCase):
         nums = [7, 2, 4]
         order = "CBA"
         self.assertEqual(answer(nums, order), "7 4 2")
-    
+
     def test2_answer(self) -> None:
+        nums = [70, 3, 2]
+        order = "ABC"
+        self.assertEqual(answer(nums, order), "2 3 70")
+
+    def test3_answer(self) -> None:
         nums = [1, 2, 30]
         order = "BAC"
         self.assertEqual(answer(nums, order), "2 1 30")
     
-    def test3_answer(self) -> None:
+    def test4_answer(self) -> None:
         nums = [10, 2, 7]
         order = "ACB"
         self.assertEqual(answer(nums, order), "2 10 7")
 
-    def test4_type(self) -> None:
-        self.assertEqual(answer("sdfk", [2,3,4]), "hello")
-    
-    def neg_int_count(self, temps:List[int]) -> int:
-        neg_count = 0
-        for n in temps:
-            if n < 0:
-                neg_count += 1
-        return neg_count
+    def test5_type(self) -> None:
+        nums = "sdfk"
+        order = "ABC"
+        with self.assertRaises(AttributeError):
+            answer(nums, order)
 
+    def test6_type(self) -> None:
+        nums = [1, 2, 3]
+        order = 10
+        with self.assertRaises(TypeError):
+            answer(nums, order)
+    
 if __name__ == "__main__":
+        
         unittest.main(verbosity=2)
